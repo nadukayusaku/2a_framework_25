7a910d1ecf7250d27c613d188f89e211f72380ee
MikanKNE <kne.mizuno0922@gmail.com>
2023-11-14 15:03:04 +0900

    kadaiA

---
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+		kadaiC();
+	}
+	void kadaiA(){
+	//ここに課題を書く。メソッド名変えてね。
+		for(int i = 1;i<=10;i++) {
+			System.out.println("現在" + i + "回目のループです。");
+		}
+	}
+}
